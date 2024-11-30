95d54f89bb131ac8cb66e40833d6811cfd408bd6
Abhishek Udiya <abhishekudiya09@gmail.com>
2024-12-01 01:48:39 +0530

    added database name

--- db/index.js
+++ db/index.js
@@ -22,7 +22,7 @@ async function connectToDatabase() {
     return db;
   } catch (err) {
     console.error("Connection failed:", err);
-    throw new Error("Database connection failed");
+    throw new Error(`Database connection failed${dbName}`);
   }
 }
 
